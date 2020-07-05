4b714ec79621ce8e81197abf8e0f5ecf54819520
Justin Nichols <ninetyonenichols@gmail.com>
2020-07-04 22:02:28 -0700

    Added placeholder files.

---
+++ translator.js
@@ -0,0 +1,6 @@
+/*
+ * Author: Justin Nichols
+ * Class: CSC337
+ * Purpose: This file contains a basic web-server. It offers an API that can be
+ *          used to translate between English, German, and Spanish.
+ */
