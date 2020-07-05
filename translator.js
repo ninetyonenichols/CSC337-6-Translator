e10d0a7d94b1d133de7b50a1ef278616bea70b06
Justin Nichols <ninetyonenichols@gmail.com>
2020-07-05 12:56:38 -0700

    Added basic server code.

--- translator.js
+++ translator.js
@@ -4,3 +4,19 @@
  * Purpose: This file contains a basic web-server. It offers an API that can be
  *          used to translate between English, German, and Spanish.
  */
+
+const http = require('http');
+
+const host = '127.0.0.1';
+const port = 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end(req.url);
+});
+
+
+server.listen(port, host, () => {
+   console.log('Web server running at http://%s:%s',host,port );
+});
