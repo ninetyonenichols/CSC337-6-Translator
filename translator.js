164871d25ac32a0f81cbc8f9575f4c20c11c8678
Justin Nichols <ninetyonenichols@gmail.com>
2020-07-05 15:11:43 -0700

    Added code for validating url.

--- translator.js
+++ translator.js
@@ -1,8 +1,8 @@
 /*
  * Author: Justin Nichols
  * Class: CSC337
- * Purpose: This file contains a basic web-server. It offers an API that can be
- *          used to translate between English, German, and Spanish.
+ * Purpose: This file contains a basic web-server. It offers an API that can 
+ *          be used to translate between English, German, and Spanish.
  */
 
 const http = require('http');
@@ -13,7 +13,21 @@ const port = 5000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(req.url);
+  var parts = req.url.split('/');
+  var languages = {
+    "e":"english",
+    "g":"german",
+    "s":"spanish",
+  }
+
+  if (parts.length == 4 && parts[1] == 'translate' && parts[2].length == 3
+        && parts[2][0] in languages && parts[2][2] in languages
+        && parts[2][0] != parts[2][2]) {
+    var translation = "asdf";
+    res.end(translation);
+  } else {
+    res.end('Invalid request.');
+  }
 });
 
 
